8bf65d9f9a33c048ff4e8ab22da21fc0f1eefeee
Danny Cao <dannycao1997@yahoo.com>
2024-03-12 23:11:54 -0400

    added comments

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,7 +1,8 @@
 import java.util.Random;
 
 
-// Create a Dice class that acts like a set of N random-tossed dies.
+// Instructions : Create a Dice class that acts like a set of N random-tossed dies.
+// 6 sides on each die
 // rolling a 1 die = singular
 // rolling a 2 dice = plural
 public class Dice {
@@ -15,8 +16,8 @@ public class Dice {
     public int tossAndSum(){
         int sum = 0;
 
-        for ( int i = 0; i < numDice; i++) {
-            sum += random.nextInt(6)+ 1;
+        for ( int i = 0; i < numDice; i++) { // rolling 2 dice
+            sum += random.nextInt(6)+ 1; //
         }
         return sum;
     }
